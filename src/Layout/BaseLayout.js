30aec1d05cf9164d93525c6a511f0f84c72f9497
leo <leozhang621@gmail.com>
2018-05-29 19:37:25 +0800

    add TopBar

--- src/Layout/BaseLayout.js
+++ src/Layout/BaseLayout.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import './BaseLayout.css'
 
+import TopBar from './TopBar/TopBar'
+
 class BaseLayout extends Component {
   render() {
     return (
       <div className="base-layout">
-
+        <div className="menu-bar">
+          menu-bar
+        </div>
+        <div className="content">
+          <TopBar/>
+        </div>
       </div>
     )
   }
---
+++ src/Layout/TopBar/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+
+function TopBar(props){
+    return (
+        <AppBar position="absolute" color="default">
+            content
+        </AppBar>
+    )
+}
+
+export default TopBar
