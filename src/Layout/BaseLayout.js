469194155e1fa3f0a2c872198851a2041c9f3fb2
leo <leozhang621@gmail.com>
2018-05-30 14:43:04 +0800

    add TopBar

--- src/Layout/BaseLayout.js
+++ src/Layout/BaseLayout.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react'
 import './BaseLayout.css'
-
-import TopBar from './TopBar/TopBar'
+import TopBar from '../Components/TopBar/TopBar'
 
 class BaseLayout extends Component {
   render() {
     return (
       <div className="base-layout">
-        <div className="menu-bar">
-          menu-bar
-        </div>
-        <div className="content">
           <TopBar/>
-        </div>
       </div>
     )
   }
